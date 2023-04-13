228ae88843d538ebb37ba7ba2edd10fdbb5d60fd
Nimra-Zahoor <Nimra.zahoor120153@gmail.com>
2023-04-13 15:00:01 +0500

    simple functionality added

---
+++ cart.js
@@ -0,0 +1,25 @@
+let Products = [
+  Basket={
+    quantity: 0,
+    discount: 100,
+    class: 'basket',
+},
+   Clock={
+    quantity: 0,
+    discount: 10,
+    class: 'clock',
+   },
+   Lamp={
+    quantity: 0,
+    discount: 60,
+    class: 'lamp',
+   },
+   Painting={
+    quantity: 0,
+    discount: 30,
+    class: 'painting',
+   }
+]
+function handleClick(){
+
+}
