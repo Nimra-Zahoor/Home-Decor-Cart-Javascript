ad3ccb286a0e201c89711668c273f6d262d823ab
Nimra-Zahoor <Nimra.zahoor120153@gmail.com>
2023-04-29 23:33:23 +0500

    the home page added

--- cart.js
+++ cart.js
@@ -97,72 +97,53 @@ function handleAddClick(param) {
 }
 
 function handleSubtractClick(param) {
-  if(count<=0){
+  if (count <= 0) {
     setTimeout(function() {
       document.getElementById("cart-notification").style.display = "none";
-    },10000000);
+    }, 10000000);
   }
-  if(count>0){
+  if (count > 0) {
     count = count - 1;
     document.getElementById("cart-notification").style.display = "block";
     document.getElementById("cart-notification").textContent = count;
 
-
-
-  if (param == bMin) {
-
-      Products[0].quantity = Products[1].quantity - 1;
-      Products[0].price = Products[1].price - 500;
-      if(Products[0].price<1000){
-      Products[0].discount = 0;
-
-      console.log("clock quantity",Products[0].quantity, "price ", Products[0].price );
+    if (param == bMin) {
+      Products[0].quantity = Products[0].quantity - 1;
+      Products[0].price = Products[0].price - 500;
+      if (Products[0].price < 1000) {
+        Products[0].discount = 0;
+      }
+      console.log("basket quantity", Products[0].quantity, "price ", Products[0].price);
       document.getElementById("basket-price").textContent = "Price: " + Products[0].price;
       document.getElementById("basket-discount").textContent = "discount:  - " + Products[0].discount;
-    }
-
-  }
-  else if (param == cMin ) {
-    if(Products[1].quantity >0 && Products[1].price >0 ){
-    Products[1].quantity = Products[1].quantity - 1;
-    Products[1].price = Products[1].price -1000;
-    if(Products[1].price<3000){
-    Products[1].discount = 0;
-    }
-    console.log("clock quantity",Products[1].quantity, "price ", Products[1].price );
-    document.getElementById("clock-price").textContent = "Price: " + Products[1].price;
-    document.getElementById("clock-discount").textContent = "discount:  - " + Products[1].discount;
-  }
-  }
-  else if (param == lMin)
-  {
-    if(Products[2].quantity >0 && Products[2].price >0 ){
-      Products[2].quantity = Products[2].quantity - 1;
-      Products[2].price = Products[2].price -2000;
-      if(Products[2].price<3000){
-      Products[2].discount = 0;
+    } else if (param == cMin) {
+      Products[1].quantity = Products[1].quantity - 1;
+      Products[1].price = Products[1].price - 1000;
+      if (Products[1].price < 1000) {
+        Products[1].discount = 0;
       }
-      console.log("clock quantity",Products[2].quantity, "price ", Products[2].price );
+      console.log("clock quantity", Products[1].quantity, "price ", Products[1].price);
+      document.getElementById("clock-price").textContent = "Price: " + Products[1].price;
+      document.getElementById("clock-discount").textContent = "discount:  - " + Products[1].discount;
+    } else if (param == lMin) {
+      Products[2].quantity = Products[2].quantity - 1;
+      Products[2].price = Products[2].price - 300;
+      if (Products[2].price < 1000) {
+        Products[2].discount = 0;
+      }
+      console.log("lamp quantity", Products[2].quantity, "price ", Products[2].price);
       document.getElementById("lamp-price").textContent = "Price: " + Products[2].price;
       document.getElementById("lamp-discount").textContent = "discount:  - " + Products[2].discount;
-    }
-  }
-    else if (param == pMin)
-    {
-
-      if(Products[3].quantity >0 && Products[3].price >0 ){
-        Products[3].quantity = Products[3].quantity - 1;
-        Products[3].price = Products[3].price -3000;
-        if(Products[3].price<3000){
+    } else if (param == pMin) {
+      Products[3].quantity = Products[3].quantity - 1;
+      Products[3].price = Products[3].price - 2000;
+      if (Products[3].price < 1000) {
         Products[3].discount = 0;
-        }
-        console.log("clock quantity",Products[3].quantity, "price ", Products[3].price );
-        document.getElementById("painting-price").textContent = "Price: " + Products[3].price;
-        document.getElementById("painting-discount").textContent = "discount:  - " + Products[3].discount;
       }
+      console.log("painting quantity", Products[3].quantity, "price ", Products[3].price);
+      document.getElementById("painting-price").textContent = "Price: " + Products[3].price;
+      document.getElementById("painting-discount").textContent = "discount:  - " + Products[3].discount;
     }
-
-}
 Total_Price = Products[0].price+Products[1].price+Products[2].price+Products[3].price;
 Total_Discount = Products[0].discount+Products[1].discount+ Products[2].discount+Products[3].discount;
 Total_Items = count;
@@ -170,4 +151,5 @@ document.getElementById("Total-Price").textContent = Total_Price;
 document.getElementById("Total-Discount").textContent = Total_Discount;
 document.getElementById("Total-Items").textContent = Total_Items;
 
+}
 }
